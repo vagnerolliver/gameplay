93e079d1b07a2f87379e9b00e8c4587a5122a3ab
vagner.oliveira <vagner.oliveira@DXTCPS010244.local>
2021-06-22 20:53:54 -0300

    aula 1 - tela do login e splash

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text} from 'react-native';
+import {ButtonIcon} from '../../components/ButtonIcon';
 import {styles} from './styles';
+import Illustration from '../../assets/illustration.png';
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Image source={Illustration} style={styles.image} resizeMode="stretch" />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}
+          suas jogatina {'\n'}
+          facilmente {'\n'}
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}
+          favoritos com seu amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com o Discord" activeOpacity={0.7} />
+      </View>
     </View>
   );
 }
