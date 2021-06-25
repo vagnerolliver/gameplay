e3063e04eb68c2d040e5de6c8fb885b7652cd1a3
Vagner Silveira de Oliveira <vagner.olliver@yahoo.com.br>
2021-06-25 17:50:37 -0300

    AuthSession

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { LogBox } from 'react-native';
+
 import { Background } from './src/components/Background';
 import { Routes } from './src/routes';
 
 import { AuthProvider } from './src/hooks/auth';
+
+LogBox.ignoreLogs([
+  'Constants.linkingUrl has been renamed to Constants.linkingUri.',
+  'Constants.manifest is null because the embedded app.config could not be read.',
+  'Constants.deviceId has been deprecated in favor of generating and storing your own ID.',
+  'Constants.installationId has been deprecated in favor of generating and storing your own ID.',
+]);
 
 export default function App() {
   return (
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Image, Text, Alert, ActivityIndicator } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Background } from '../../components/Background';
 
 import Illustration from '../../assets/illustration.png';
+import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
 import { useAuth } from '../../hooks/auth';
 
 export function SignIn() {
-  const navigation = useNavigation();
+  const { loading, signIn } = useAuth();
 
-  const { user } = useAuth();
-  console.log('user', user);
-
-  function handleSignIn() {
-    navigation.navigate('Home');
+  async function handleSignIn() {
+    try {
+      await signIn();
+    } catch (error) {
+      console.log('SignIn Error', error);
+    }
   }
 
   return (
@@ -40,7 +41,11 @@ export function SignIn() {
             favoritos com seu amigos
           </Text>
 
-          <ButtonIcon title="Entrar com o Discord" onPress={handleSignIn} />
+          {loading ? (
+            <ActivityIndicator color={theme.colors.primary} />
+          ) : (
+            <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
+          )}
         </View>
       </View>
     </Background>
